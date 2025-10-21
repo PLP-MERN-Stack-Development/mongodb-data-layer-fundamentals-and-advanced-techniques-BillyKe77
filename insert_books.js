f443ac6f721d23283d8512464c30285a93083c15
BillyKe77 <blykang77@gmail.com>
2025-10-21 14:00:53 +0300

    addition of books into the collection

--- insert_books.js
+++ insert_books.js
@@ -4,7 +4,7 @@
 const { MongoClient } = require('mongodb');
 
 // Connection URI (replace with your MongoDB connection string if using Atlas)
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb+srv://Billy_Kang001:0LDaVafoNmogiA0L@cluster0.ohc1u4t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 // Database and collection names
 const dbName = 'plp_bookstore';
